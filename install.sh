98906e7fe07e03d6dc58ea5df6473235fa8565ff
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-04-04 22:51:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,11 @@
 
 cp *.sublime-build ~/.config/sublime-text-3/Packages/User
 
-echo "Successfully copied"
+tput setaf 2; ls | egrep '\.sublime-build$' | cut -f 1 -d '.'
+
+tput setaf 1; echo "Sucessfully installed in your Sublime"
+
+tput setaf 2; echo "Go Tools>Build System in your Sublime set your compiler/interpretor or Automatic"
+
+
+tput setaf 1; echo  "Script Author: RIZWAN AHMAD\nGithub:http://github.com/rizwansoaib"

9d27378b5045f9cc01d6ecf5f7ad1ca03fdf70fc
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-04-04 22:52:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,4 @@ tput setaf 1; echo "Sucessfully installed in your Sublime"
 tput setaf 2; echo "Go Tools>Build System in your Sublime set your compiler/interpretor or Automatic"
 
 
-tput setaf 1; echo  "Script Author: RIZWAN AHMAD\nGithub:http://github.com/rizwansoaib"
+tput setaf 1; echo  "Script Author: RIZWAN AHMAD\nGithub: http://github.com/rizwansoaib"

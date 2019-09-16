1c0dbc5bb8d5ee5f35d46bcc81f17f8c3d3b59a2
RIZWAN AHMAD <rizwansoaib@gmail.com>
2019-09-16 19:17:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,15 @@ tput setaf 2; ls | egrep '\.sublime-build$' | cut -f 1 -d '.'
 
 tput setaf 1; echo "Sucessfully installed in your Sublime"
 
+tput setaf 2; echo "Now installing Compiler and interpretor for C,C++,java,Python"
+
+
+tput setaf 1; echo "Please Enter password to proceed Next: "
+
+sudo apt install g++ default-jre default-jdk python3 -y
+
+
+
 tput setaf 2; echo "Go Tools>Build System in your Sublime set your compiler/interpretor or Automatic"
 
 tput sgr0; echo "Build and run Programs from Sublime press ctrl+Shift+B"
